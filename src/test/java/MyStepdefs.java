aa6a1643019e6ee487a26166a215312e394d684d
Mateusz Polanski <mateusz_polanski@epam.com>
2021-11-04 08:42:27 +0100

    Commit

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,49 @@
+public class MyStepdefs {
+    @io.cucumber.java.en.Given("^Valid <credentials>$")
+    public void validCredentials() {
+    }
+
+    @io.cucumber.java.en.When("^I send POST request to API$")
+    public void iSendPOSTRequestToAPI() {
+    }
+
+    @io.cucumber.java.en.Then("^I get response code (\\d+)$")
+    public void iGetResponseCode(int arg0) {
+    }
+
+    @io.cucumber.java.en.And("^I receive a token$")
+    public void iReceiveAToken() {
+    }
+
+    @io.cucumber.java.en.Given("^The booking list is not empty$")
+    public void theBookingListIsNotEmpty() {
+    }
+
+    @io.cucumber.java.en.When("^I send GET request to /URL$")
+    public void iSendGETRequestToURL() {
+    }
+
+    @io.cucumber.java.en.Then("^I get response code (\\d+) with an Array of objects with unique IDs$")
+    public void iGetResponseCodeWithAnArrayOfObjectsWithUniqueIDs(int arg0) {
+    }
+
+    @io.cucumber.java.en.Given("^The booking with <id> exist$")
+    public void theBookingWithIdExist() {
+    }
+
+    @io.cucumber.java.en.Then("^I receive <response>$")
+    public void iReceiveResponse() {
+    }
+
+    @io.cucumber.java.en.Given("^Booking <request>$")
+    public void bookingRequest() {
+    }
+
+    @io.cucumber.java.en.When("^I send POST request to /URL$")
+    public void iSendPOSTRequestToURL() {
+    }
+
+    @io.cucumber.java.en.When("^I send DELETE request to /URL$")
+    public void iSendDELETERequestToURL() {
+    }
+}
