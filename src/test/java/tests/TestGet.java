43a6f9792d78dc38cbd88a771975e06f510d20c1
Mateusz_Polanski <mateopolanski>
2021-11-05 08:02:05 +0100

    update Tests

--- src/test/java/tests/TestGet.java
+++ src/test/java/tests/TestGet.java
@@ -1,4 +1,23 @@
 package tests;
 
-public class TestGet {
+import org.junit.jupiter.api.Test;
+import utilitity.BaseTests;
+
+import static org.apache.http.HttpStatus.SC_OK;
+
+public class TestGet extends BaseTests {
+
+    @Test
+    public void getEmployees() {
+        response = calls.get();
+        softly.assertThat(response.statusCode()).isEqualTo(SC_OK);
+        softly.assertThat(response.jsonPath().get("data[0].firstname").toString()).isEqualTo("Susan");
+        softly.assertThat(response.jsonPath().get("data[0].lastname").toString()).isEqualTo("Jackson");
+        softly.assertThat(response.jsonPath().get("data[0].totalprice").toString()).isEqualTo(322);
+        softly.assertThat(response.jsonPath().get("data[1].checkin").toString()).isEqualTo("2017-08-05");
+        softly.assertThat(response.jsonPath().get("data[1].checkout").toString()).isEqualTo("2019-11-03");
+        softly.assertAll();
+
+    }
+
 }
--- src/test/java/utilitity/RestController.java
+++ src/test/java/utilitity/RestController.java
@@ -2,12 +2,12 @@ package utilitity;
 
 import io.restassured.response.Response;
 import model.PostRequest;
-import io.restassured.RestAssured;
 import utilitity.RestSteps;
+import static io.restassured.RestAssured.given;
 
 public class RestController  {
     public Response post(PostRequest body) {
-        return super
+        return
                 .given()
                 .body(body)
                 .basePath("")
@@ -20,10 +20,10 @@ public class RestController  {
     }
 
     public Response get() {
-        return super
+        return
                 .given()
                 .when()
-                .basePath("/id")
+                .basePath("/9")
                 .log().all()
                 .get()
                 .then()
